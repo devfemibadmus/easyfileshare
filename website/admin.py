aa61f90f45f92e5672fc8c15ca2b6414af2511ed
devfemibadmus <devfemibadmus@gmail.com>
2024-01-09 09:44:58 +0100

    re-db

--- website/admin.py
+++ website/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from .models import FileManager
 
-# Register your models here.
+class FileManagerAdmin(admin.ModelAdmin):
+    list_display = ('file_upload', 'user', 'upload_date')
+    list_filter = ('upload_date',)
+    search_fields = ('file_upload', 'user__username')
+
+    def get_ordering(self, request):
+        # Default ordering by upload_date in descending order
+        ordering = ['-upload_date']
+
+        # Check if 'sort' parameter is present in the request
+        if 'sort' in request.GET:
+            sort_param = request.GET['sort']
+
+            # Customize ordering based on the sort parameter
+            if sort_param == '3days_ago':
+                ordering = ['-upload_date']
+            elif sort_param == 'recently_uploaded':
+                ordering = ['upload_date']
+
+        return ordering
+
+admin.site.register(FileManager, FileManagerAdmin)
--- website/models.py
+++ website/models.py
@@ -1,13 +1,17 @@
-from django.db import models
-from django.contrib.auth.models import User
 import uuid
+from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
 class FileManager(models.Model):
     user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
-    user_cache_key = models.CharField(max_length=255, blank=True, null=True)
     file_upload = models.FileField(upload_to='')
-    auto_delete = models.BooleanField(default=False)
     shareable_link = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
+    upload_date = models.DateTimeField(default=timezone.now)
 
     def __str__(self):
         return self.file_upload.name
+
+    def delete(self, *args, **kwargs):
+        self.file_upload.delete()
+        super().delete(*args, **kwargs)
