aea116341de4191ca3924022595642890372d7b0
devfemibadmus <devfemibadmus@gmail.com>
2024-01-09 09:46:50 +0100

    update-for-admin

--- easyfileshare/urls.py
+++ easyfileshare/urls.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('', include("website.urls")),
     path('admin/', admin.site.urls),
+    path('', include("website.urls")),
 ]
