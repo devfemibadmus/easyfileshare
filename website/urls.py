c9c6706157263a782197cf2e6e0d50377cd5c1d2
devfemibadmus <devfemibadmus@gmail.com>
2024-01-12 23:33:53 +0100

    404-page

--- website/urls.py
+++ website/urls.py
@@ -9,7 +9,5 @@ urlpatterns = [
     path('file_upload/', views.file_upload, name='file_upload'),
     path('file_delete/<str:shareable_link>/', views.file_delete, name='file_delete'),
     path('view_file/<str:shareable_link>', views.view_file, name='view_file'),
-    
-    # Redirect any other path to the home page
     re_path(r'^.*$', RedirectView.as_view(pattern_name='home', permanent=False)),
 ]
